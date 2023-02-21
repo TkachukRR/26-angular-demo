2e8f05a869d3f524216ce48e812a527c0b890092
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-21 16:46:46 +0100

    #002 relocated sub tasks to new component task1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '35-angular-demo';
-  arr = ['asb', 1, true];
-  obj = { par: 'slp' };
-  changeableURL = 'https://www.bsmu.edu.ua/wp-content/uploads/2019/03/6923.jpg';
-  inputValue = '';
-  clickCounter = 0;
-
-  constructor() {
-    setTimeout(() => {
-      this.changeableURL =
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReMYSJLBgXEa9WWe5VtP1mcZOt4d7eS78mJZOrO9XhcEjC90ZzJl_kMTIxVsvWB1cARC8&usqp=CAU';
-    }, 3000);
-  }
-
-  onInputChange(event?: any) {
-    this.inputValue = event.target.value;
-  }
-
-  onHandleClick() {
-    this.clickCounter++;
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { Post2Component } from './post2/post2.component';
+import { Task1Component } from './task1/task1.component';
 
 @NgModule({
-  declarations: [AppComponent, PostComponent, Post2Component],
+  declarations: [AppComponent, PostComponent, Post2Component, Task1Component],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
