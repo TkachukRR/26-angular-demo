bafd1312d1bb17176883194c9ac605704593a4fc
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-21 16:50:20 +0100

    #002 added twoWayBinding component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { Post2Component } from './post2/post2.component';
 import { Task1Component } from './task1/task1.component';
+import { TowWayBindingComponent } from './tow-way-binding/tow-way-binding.component';
 
 @NgModule({
-  declarations: [AppComponent, PostComponent, Post2Component, Task1Component],
+  declarations: [AppComponent, PostComponent, Post2Component, Task1Component, TowWayBindingComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/tow-way-binding/tow-way-binding.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tow-way-binding',
+  templateUrl: './tow-way-binding.component.html',
+  styleUrls: ['./tow-way-binding.component.scss']
+})
+export class TowWayBindingComponent {
+
+}
