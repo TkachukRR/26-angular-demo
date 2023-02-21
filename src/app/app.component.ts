f1e353db37af7122487825946d68f0ec43f9b7e7
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-21 14:07:37 +0100

    #002 Added interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = '35-angular-demo';
+  arr = ['asb', 1, true];
+  obj = { par: 'slp' };
 }
