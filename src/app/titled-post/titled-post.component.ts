ba126f62ce3c12013322dc5069ac5f87c8a1b61f
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-22 21:41:58 +0100

    #009 added abiliti to dom element

--- src/app/titled-post/titled-post.component.ts
+++ src/app/titled-post/titled-post.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  ElementRef,
+  Input,
+  OnInit,
+} from '@angular/core';
 import { Post } from '../app.component';
 
 @Component({
@@ -8,8 +14,11 @@ import { Post } from '../app.component';
 })
 export class TitledPostComponent implements OnInit {
   @Input() post: Post;
+  @ContentChild('info', { static: true }) infoRef: ElementRef;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log(this.infoRef.nativeElement);
+  }
 }
