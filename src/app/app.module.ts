fa1970cff291e8b8be1c49b4530ad953e815109c
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-22 14:26:17 +0100

    #009 added components: titled-post, create-post-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
-import { Post2Component } from './post2/post2.component';
-import { Task1Component } from './task1/task1.component';
-import { TowWayBindingComponent } from './tow-way-binding/tow-way-binding.component';
+// import { PostComponent } from './post/post.component';
+// import { Post2Component } from './post2/post2.component';
+// import { Task1Component } from './task1/task1.component';
+// import { TowWayBindingComponent } from './tow-way-binding/tow-way-binding.component';
 import { FormsModule } from '@angular/forms';
-import { StyledComponent } from './styled/styled.component';
-import { ClassComponent } from './class/class.component';
-import { IfElseComponent } from './if-else/if-else.component';
-import { SwitchComponent } from './switch/switch.component';
-import { ForComponent } from './for/for.component';
+// import { StyledComponent } from './styled/styled.component';
+// import { ClassComponent } from './class/class.component';
+// import { IfElseComponent } from './if-else/if-else.component';
+// import { SwitchComponent } from './switch/switch.component';
+// import { ForComponent } from './for/for.component';
+import { TitledPostComponent } from './titled-post/titled-post.component';
+import { CreatePostFormComponent } from './create-post-form/create-post-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
-    Post2Component,
-    Task1Component,
-    TowWayBindingComponent,
-    StyledComponent,
-    ClassComponent,
-    IfElseComponent,
-    SwitchComponent,
-    ForComponent,
+    // PostComponent,
+    // Post2Component,
+    // Task1Component,
+    // TowWayBindingComponent,
+    // StyledComponent,
+    // ClassComponent,
+    // IfElseComponent,
+    // SwitchComponent,
+    // ForComponent,
+    TitledPostComponent,
+    CreatePostFormComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
---
+++ src/app/titled-post/titled-post.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-titled-post',
+  templateUrl: './titled-post.component.html',
+  styleUrls: ['./titled-post.component.scss']
+})
+export class TitledPostComponent {
+
+}
