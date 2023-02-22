aa492b69c3c69d0309bd265ffdaaad009cf573f1
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-22 23:19:52 +0100

    #010 Change element style by derective

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,6 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appStyle]',
 })
-export class StyleDirective {}
+export class StyleDirective {
+  constructor(private elRef: ElementRef, private render: Renderer2) {
+    console.log(elRef);
+    elRef.nativeElement.style.color = 'red';
+    this.render.setStyle(this.elRef.nativeElement, 'color', 'green');
+  }
+}
