a2cc89b30bed1b6f623a32d4470a1b44f631a23f
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-22 15:18:04 +0100

    #009 added ngFor of for titlePost component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '35-angular-demo';
+
+  posts: Array<Post> = [
+    { title: 'post 1', text: 'Lorem ipsum dolor sit', id: 1 },
+    { title: 'post 2', text: 'Amet consectetur adipisicing elit.', id: 1 },
+  ];
 }
--- src/app/titled-post/titled-post.component.ts
+++ src/app/titled-post/titled-post.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from '../app.component';
 
 @Component({
   selector: 'app-titled-post',
   templateUrl: './titled-post.component.html',
-  styleUrls: ['./titled-post.component.scss']
+  styleUrls: ['./titled-post.component.scss'],
 })
-export class TitledPostComponent {
+export class TitledPostComponent implements OnInit {
+  @Input() post: Post;
 
+  constructor() {}
+
+  ngOnInit() {}
 }
