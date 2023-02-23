0ba5705a7224b16886ae8d83adfb46d33699d81a
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-23 15:28:52 +0100

    #011 Added pipe json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,6 @@ export class AppComponent {
   e: number = Math.E;
   str: string = 'new string example';
   date: Date = new Date();
+  float = 0.42;
+  obj = { a: 1, b: 2, c: { c1: 3, c3: { d: 0 } } };
 }
