62a5f0327605e92b68e1619fc48d50fd8fb387c1
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-23 16:37:40 +0100

    #011 Generated filter pipe by CLI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,4 +19,21 @@ export class AppComponent {
   date: Date = new Date();
   float = 0.42;
   obj = { a: 1, b: 2, c: { c1: 3, c3: { d: 0 } } };
+
+  posts: Array<Post> = [
+    {
+      title: 'Beer',
+      text: 'The best beer',
+    },
+    {
+      title: 'milk',
+      text: 'The best milk',
+    },
+    {
+      title: 'cola',
+      text: 'The best drink',
+    },
+  ];
+
+  search = '';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { MultByPipe } from './pipes/mult-by.pipe';
 import { ExMarksPipe } from './pipes/ex-marks.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
-  declarations: [AppComponent, MultByPipe, ExMarksPipe],
+  declarations: [AppComponent, MultByPipe, ExMarksPipe, FilterPipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
