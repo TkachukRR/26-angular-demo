04fb0981f0d01bd247583f691f02f232802db7d9
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-23 11:33:09 +0100

    #010 Added posibility change element style by derective and @HostListener

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appStyle]',
@@ -7,6 +7,20 @@ export class StyleDirective {
   constructor(private elRef: ElementRef, private render: Renderer2) {
     console.log(elRef);
     elRef.nativeElement.style.color = 'red';
-    this.render.setStyle(this.elRef.nativeElement, 'color', 'green');
+    setTimeout(() => {
+      this.render.setStyle(this.elRef.nativeElement, 'color', 'green');
+    }, 3000);
+  }
+
+  @HostListener('click', ['$event.target']) onClick(event: Event) {
+    console.log(event);
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.render.setStyle(this.elRef.nativeElement, 'color', 'blue');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.render.setStyle(this.elRef.nativeElement, 'color', null);
   }
 }
