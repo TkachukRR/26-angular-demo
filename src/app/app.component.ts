b5ebdef4b0193aa6f6806a9e8e3dc6252969b8a4
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-24 09:40:53 +0100

    #012 Prepared for services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,9 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-
-export interface Post {
-  title: string;
-  text: string;
-  id?: number;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Angular pipes';
-
-  e: number = Math.E;
-  str: string = 'new string example';
-  date: Date = new Date();
-  float = 0.42;
-  obj = { a: 1, b: 2, c: { c1: 3, c3: { d: 0 } } };
-
-  posts: Array<Post> = [
-    {
-      title: 'Beer',
-      text: 'The best beer vvv',
-    },
-    {
-      title: 'milk',
-      text: 'The best milk aaa',
-    },
-    {
-      title: 'cola',
-      text: 'The best drink ccc',
-    },
-  ];
-
-  search = '';
-  searchField = 'title';
-
-  promise: Promise<string> = new Promise<string>((resolve) =>
-    setTimeout(() => {
-      resolve('Promise resolved');
-    }, 3000)
-  );
-
-  dateNow: Observable<Date> = new Observable((obs) => {
-    setInterval(() => {
-      obs.next(new Date());
-    }, 1000);
-  });
-
-  addPost() {
-    this.posts.unshift({
-      title: 'sprite',
-      text: 'Good drink ddd',
-    });
-  }
+  title = 'Angular services';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { MultByPipe } from './pipes/mult-by.pipe';
-import { ExMarksPipe } from './pipes/ex-marks.pipe';
-import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
-  declarations: [AppComponent, MultByPipe, ExMarksPipe, FilterPipe],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
