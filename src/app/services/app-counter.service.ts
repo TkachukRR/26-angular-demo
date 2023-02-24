112341972807bdf9844bc988435d14f0672af245
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-24 10:37:31 +0100

    #012 Added services interaction

--- src/app/services/app-counter.service.ts
+++ src/app/services/app-counter.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
+import { LogService } from './log.service';
 @Injectable({
   providedIn: 'root',
 })
 export class AppCounterService {
   counter = 0;
 
+  constructor(private logService: LogService) {}
+
   increase() {
     this.counter++;
+    this.logService.log(this.counter.toString());
   }
 
   decrease() {
     this.counter--;
+    this.logService.log(this.counter.toString());
   }
 }
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LogService {
-
-  constructor() { }
+  log(text: string) {
+    console.log(`log: ${text}`);
+  }
 }
