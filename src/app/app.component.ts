88ad1abdff212d2b4e8b2581686b6603a589801f
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-01 22:48:27 +0100

    #015 Get data from remote server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface Todo {
+  completed: boolean;
+  title: string;
+  id?: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular HTTP client';
+
+  todos: Array<Todo> = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .subscribe((todoList) => {
+        console.log(todoList);
+        this.todos = todoList;
+      });
+  }
 }
