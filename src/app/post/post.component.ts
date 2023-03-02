8a906fe8211ebaf3f04fc0d71233602ff727060a
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-02 23:27:11 +0100

    016 Added loading data by params

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,24 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import {ActivatedRoute} from "@angular/router";
+import {Post, PostsService} from "../posts.service";
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit{
+  post: Post
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostsService) {
+  }
 
+  ngOnInit(): void {
+    this.route.params.subscribe((params)=> {
+      console.log(params)
+      // @ts-ignore
+      this.post = this.postService.getById(+params?.['id'])
+    })
+  }
 }
