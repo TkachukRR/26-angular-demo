740be1cdbc3c2d423c6bee001ae857ecd7b02942
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-02 23:31:50 +0100

    016 Added posability load post from component

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core'
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Post, PostsService} from "../posts.service";
 
 @Component({
@@ -11,6 +11,7 @@ export class PostComponent implements OnInit{
   post: Post
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private postService: PostsService) {
   }
 
@@ -20,5 +21,9 @@ export class PostComponent implements OnInit{
       // @ts-ignore
       this.post = this.postService.getById(+params?.['id'])
     })
+  }
+
+  loadPost() {
+  this.router.navigate(['/posts', 44])
   }
 }
