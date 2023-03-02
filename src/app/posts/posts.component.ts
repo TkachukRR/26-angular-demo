e8bb81202af1a51be0352305a8ab276d60605451
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-02 23:52:10 +0100

    016 Added showIds button logic by queryParams

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,11 +1,24 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 import {PostsService} from '../posts.service'
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent {
-  constructor(public postsService: PostsService) {}
+export class PostsComponent implements OnInit{
+  showIds = false
+  constructor(
+    public postsService: PostsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit():void {
+    this.route.queryParams.subscribe((params)=> {
+        console.log(params)
+        this.showIds = !!params?.['showIds']
+      }
+    )
+  }
 }
