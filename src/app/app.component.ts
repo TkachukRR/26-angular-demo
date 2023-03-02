4ff9a9349c73ed7010d33a91565db4f266440046
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-02 09:47:09 +0100

    #015 Created todo.service and moved logic to service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { delay } from 'rxjs';
+import { Todo, TodosService } from './todos.service';
 
-export interface Todo {
-  completed: boolean;
-  title: string;
-  id?: any;
-}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,7 +14,7 @@ export class AppComponent implements OnInit {
   todoTitle = '';
   loading = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private todosService: TodosService) {}
 
   ngOnInit() {
     this.fetchTodos();
@@ -35,37 +30,29 @@ export class AppComponent implements OnInit {
       completed: false,
     };
 
-    this.http
-      .post<Todo>('https://jsonplaceholder.typicode.com/todos', {
-        ...newTodo,
-      })
-      .subscribe((todo) => {
-        console.log(todo);
-        this.todos.push(todo);
-        this.todoTitle = '';
-      });
+    this.todosService.addTodo(newTodo).subscribe((todo) => {
+      console.log(todo);
+      this.todos.push(todo);
+      this.todoTitle = '';
+    });
   }
 
   fetchTodos() {
     this.loading = true;
-    this.http
-      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
-      .pipe(delay(2000))
-      .subscribe((todoList) => {
-        console.log(todoList);
-        this.todos = todoList;
-        this.loading = false;
-      });
+
+    this.todosService.fetchTodos().subscribe((todoList) => {
+      console.log(todoList);
+      this.todos = todoList;
+      this.loading = false;
+    });
   }
 
   removeTodo(id: number) {
-    this.http
-      .delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .subscribe((deletedTodo) => {
-        console.log(deletedTodo);
-        console.log(this.todos);
-        this.todos = this.todos.filter((todo) => todo.id !== id);
-        console.log(this.todos);
-      });
+    this.todosService.removeTodo(id).subscribe((deletedTodo) => {
+      console.log(deletedTodo);
+      console.log(this.todos);
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+      console.log(this.todos);
+    });
   }
 }
---
+++ src/app/todos.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { delay, Observable } from 'rxjs';
+
+export interface Todo {
+  completed: boolean;
+  title: string;
+  id?: any;
+}
+
+@Injectable({ providedIn: 'root' })
+export class TodosService {
+  constructor(private http: HttpClient) {}
+
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', {
+      ...todo,
+    });
+  }
+
+  fetchTodos(): Observable<Todo[]> {
+    return this.http
+      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .pipe(delay(1000));
+  }
+
+  removeTodo(id: number): Observable<void> {
+    return this.http.delete<void>(
+      `https://jsonplaceholder.typicode.com/todos/${id}`
+    );
+  }
+}
