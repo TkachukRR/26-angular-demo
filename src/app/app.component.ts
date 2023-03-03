7b064cb0e735ad1d6b5460d9f3f4c3104586f935
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-03 12:08:47 +0100

    #016 Added login guards for posts page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "./auth.service";
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Angular Routing';
+  constructor(public auth: AuthService) {
+
+  }
   ngOnInit() {
   }
 }
