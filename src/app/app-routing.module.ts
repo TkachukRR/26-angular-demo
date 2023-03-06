a2ef53ecb08d77edbe198ec079845d34a6a04644
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-06 09:48:16 +0100

    #017 Moved routes to const routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import {NgModule} from '@angular/core'
-import {RouterModule} from '@angular/router'
+import {RouterModule, Routes} from '@angular/router'
 import {HomePageComponent} from './home-page/home-page.component'
 
-@NgModule({
-  imports: [RouterModule.forRoot([
-    {path: '', component: HomePageComponent, pathMatch: 'full'},
-    {path: 'about', loadChildren:  () => import('./about-page/about-page.module').then(m => m.AboutPageModule)},
+const routes: Routes = [
+  {path: '', component: HomePageComponent, pathMatch: 'full'},
+  {path: 'about', loadChildren:  () => import('./about-page/about-page.module').then(m => m.AboutPageModule)},
+]
 
-  ])],
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
