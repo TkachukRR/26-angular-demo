6ce8d9761953ed10c1fbb2d92b3d373e14823a4c
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-06 09:43:55 +0100

    #017 Added lazyloading module for about-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,12 @@
 import {NgModule} from '@angular/core'
 import {RouterModule} from '@angular/router'
 import {HomePageComponent} from './home-page/home-page.component'
-import {AboutPageComponent} from './about-page/about-page.component'
-import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component'
 
 @NgModule({
   imports: [RouterModule.forRoot([
-    {path: 'about', component: AboutPageComponent, children: [
-      {path: 'extra', component: AboutExtraPageComponent}
-    ]},
-    {path: '', component: HomePageComponent, pathMatch: 'full'}
+    {path: '', component: HomePageComponent, pathMatch: 'full'},
+    {path: 'about', loadChildren:  () => import('./about-page/about-page.module').then(m => m.AboutPageModule)},
+
   ])],
   exports: [RouterModule]
 })
