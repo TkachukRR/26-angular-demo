da31180c998a50cd4f5f2ad828b5668456504bdb
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-06 08:38:32 +0100

    #017 prepared files for new task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import {AuthService} from "./auth.service";
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'Angular Routing';
-  constructor(public auth: AuthService) {
-
-  }
-  ngOnInit() {
-  }
+export class AppComponent {
+  constructor() {}
 }
+
